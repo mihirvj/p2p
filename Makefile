ba69622f56e4b95dd35c1d10bcee91934178346e
Mihir Joshi <mihir.coldplay@gmail.com>
2013-11-08 07:17:00 -0500

    compiler flag for log

--- Makefile
+++ Makefile
@@ -15,13 +15,13 @@ all: boot speer cpeer
 	mv sock/*.o sock/objs/
 
 boot: ${S_OBJS} ${HEADERS}
-	${GCC} -o $(EXEC_PATH)/boot boot.c ${S_OBJS} $(PLIB)
+	${GCC} -o $(EXEC_PATH)/boot boot.c ${S_OBJS} $(PLIB) ${FLAGS}
 
 speer: ${S_OBJS} ${HEADERS}
-	${GCC} -o $(EXEC_PATH)/speer speer.c ${S_OBJS}
+	${GCC} -o $(EXEC_PATH)/speer speer.c ${S_OBJS} ${FLAGS}
 
 cpeer: ${C_OBJS} ${HEADERS}
-	${GCC} -o $(EXEC_PATH)/cpeer cpeer.c ${C_OBJS}
+	${GCC} -o $(EXEC_PATH)/cpeer cpeer.c ${C_OBJS} ${FLAGS}
 
 %.o: %.c ${HEADERS}
 	${GCC} -c $< -o $@
--- boot.c
+++ boot.c
@@ -9,7 +9,6 @@
 #include "ds/ll/ll.h"
 #include<signal.h>
 #include<pthread.h>
-#include<semaphore.h>
 
 #define PORT 7752
 #define BUFSIZE 256
@@ -25,10 +24,12 @@ void clear(int signum)
 
   close_sock(sock);
 
+#ifdef APP
   printf("[log] list destroyed\n");
 
   printf("[log] server socket closed\n");
- 
+#endif
+
   exit(0);
 }
 
@@ -76,39 +77,55 @@ void *handle_con(void *arg)
  char buf[BUFSIZE];
  int csock = *(int *) arg;
 
+#ifdef APP
  printf("[log] I am in child process of csock: %d", csock);
+#endif
 
  // read from client
  read_from(csock, buf, BUFSIZE);
 
+#ifdef APP
  printf("\n[log] client says: %s\n", buf);
+#endif
 
 //fflush(stdout);
  if(strcmp(buf, "add") == 0)
  {
+#ifdef APP
    printf("[log] comparison success rfc head: %ld\n", &rfc_head);
+#endif
 
    add_node(&rfc_head, csock, 0);
 
+#ifdef APP
    printf("[log] node added\n");
+#endif
 
    traverse(rfc_head);
 
+#ifdef APP
    printf("[log] list traversed\n");
+#endif
  }
  else
  {
+#ifdef APP
    printf("[log] comparison fail\n");
+#endif
  }
 
+#ifdef APP
  printf("[log] writing to client %s\n", buf);
+#endif
 
  // echo reply to client
  write_to(csock, buf, BUFSIZE);
 
  close_sock(csock);
 
+#ifdef APP
  printf("[log] client socket closed %d\n", csock);
+#endif
 
  pthread_exit(NULL);
 }
